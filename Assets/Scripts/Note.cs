6646eee4a19b95afbd524aff3d8d0611b23cfcc8
Andrii Shcherbakov <andrey.shcherbakov05@gmail.com>
2026-03-16 23:46:16 +0200

    Implement smooth Note spawn

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -12,11 +12,21 @@ public class Note : MonoBehaviour
 	[SerializeField] private float _targetRadius;
 	[SerializeField] private Vector2 _direction;
 
+	[SerializeField] private SpriteRenderer _spriteRenderer;
+
 	public System.Action<Note, NoteGrade> OnNoteFinished;
 
 	public float Progress
 	{
 		get { return (Time.time - _spawnTime) / _travelDuration; }
+	}
+
+	private void Start()
+	{
+		// Hide the sprite - it shows up smoothly on changing Progress
+		Color transparent = _spriteRenderer.color;
+		transparent.a = 0;
+		_spriteRenderer.color = transparent;
 	}
 
 	public void Init(float travelDuration, float targetRadius, float angleDegrees)
@@ -46,6 +56,14 @@ public class Note : MonoBehaviour
 	private void Update()
 	{
 		transform.position = _direction * (Progress * _targetRadius);
+		transform.localScale = Vector3.one * Mathf.Lerp(0.5f, 1f, Progress);
+
+		if (_spriteRenderer)
+		{
+			Color newTransparency = _spriteRenderer.color;
+			newTransparency.a = Progress;
+			_spriteRenderer.color = newTransparency;
+		}
 
 		if (Progress >= 1.2f)
 		{
